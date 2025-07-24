5a7100d439248d19fc0eef29dbe3d3a71163c943
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-07-24 10:15:01 +0500

    Initial Class Structure for Account Holder

---
+++ src/Account_Holder.cpp
@@ -0,0 +1,12 @@
+#include "Account_Holder.h"
+
+int AccountHolder::account_holder_count = 0;
+
+AccountHolder::AccountHolder() : Person() {
+    this->setType("AccountHolder");
+    AccountHolder::account_holder_count++;
+}
+
+int AccountHolder::get_count() {
+    return AccountHolder::account_holder_count;
+}
