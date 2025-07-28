4747dd3500e6c1eeb3a5798cf3daca17d6449b72
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-07-29 00:13:10 +0500

    Implemented Main Menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
 #include "../include/Utility/UI/Utility_UI.h"
 #include <iostream>
+#include <vector>
+#include <conio.h>
 
 class Bank
 {
 private:
-    void main_menu() {
-        Utilit
+    void create_account()
+    {
+        Utility_UI::print_header("Account Creation");
+    }
+    void exit_bank()
+    {
+        Utility_UI::print_success_message("Thank You for Using Bank Mangement System");
+        Utility_UI::print_info_box("Author: Maaz Bin Asif");
+        Utility_UI::print_info_box("Linkedin -> @maazbinasif");
+        Utility_UI::print_info_box("Instagram -> @maaz.binasif");
+        getch();
+        return;
+    }
+    void main_menu()
+    {
+        int choice{0};
+        choice = Utility_UI::print_menu_box("Bank Mangement System", std::vector<string>{"Login", "Create Account", "Exit"});
+        if (choice == 3)
+        {
+            exit_bank();
+        }
+        else if (choice == 2)
+        {
+            create_account();
+        }
     }
 
 public:
