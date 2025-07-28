29df5aad94d571657f09a55a3ac1167b5966c333
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-07-28 23:54:23 +0500

    Basic main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "../include/Utility/UI/Utility_UI.h"
+#include <iostream>
+
+class Bank
+{
+private:
+    void main_menu() {
+        Utilit
+    }
+
+public:
+    void run()
+    {
+        main_menu();
+    }
+};
+
+int main()
+{
+    Bank Bank;
+    Bank.run();
+    return 0;
+}
