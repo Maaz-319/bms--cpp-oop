1f7f23bdd802dd504850205133a6a0c90b4d852d
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-07-29 13:05:57 +0500

    Updated paths in built bat file

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Compiling Bank Management System...
-if exist bms.exe del bms.exe
-g++ -std=c++17 -I./include src/main.cpp src/Utility/UI/Utility_UI.cpp -o build/bms.exe
+if exist build\bms.exe del build\bms.exe
+g++ -std=c++17 -I./include src/main.cpp src/Utility/UI/Utility_UI.cpp src/Person/Person.cpp src/Person/Account_Holder.cpp src/Account/Account.cpp src/Account/Current_Account.cpp src/Bank/Bank.cpp -o build/bms.exe
 if %errorlevel% equ 0 (
-    echo Build successful! Run bms.exe to start the program.
+    echo Build successful! Run build/bms.exe to start the program.
 ) else (
     echo Build failed!
 )
