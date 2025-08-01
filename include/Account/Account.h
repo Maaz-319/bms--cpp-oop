d1c0a6ebd755c28046f57e978f7ac749fa5d7fc1
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-08-01 13:43:13 +0500

    Implemented Limits Variables

--- include/Account/Account.h
+++ include/Account/Account.h
@@ -12,30 +12,31 @@ protected:
     string type;
     string pin;
     Person *owner;
+    double onetime_transaction_limit;
+    double minimum_transaction_limit;
 
 public:
     Account();
 
     // Getters
-    virtual string getAccNo();
-    virtual double getBalance();
-    virtual string getType();
-    virtual string getPin();
-    virtual Person *getOwner();
+    string getAccNo();
+    double getBalance();
+    string getType();
+    string getPin();
+    Person *getOwner();
+    double getOneTimeTransactionLimit();
+    double getMinimumTransactionLimit();
 
     // Setters
-    virtual void setAccNo(const string &acc_no);
-    virtual void setBalance(double balance);
-    virtual void setType(const string &type);
-    virtual void setPin(const string &pin);
-    virtual void setOwner(Person *owner);
+    void setAccNo(const string &acc_no);
+    void setBalance(double balance);
+    void setType(const string &type);
+    void setPin(const string &pin);
+    void setOwner(Person *owner);
+    void setOneTimeTransactionLimit(double amount);
+    void setMinimumTransactionLimit(double amount);
 
-    // Transaction methods
-    virtual void deposit(float amount) = 0;
-    virtual void withdraw(float amount) = 0;
-    virtual void transfer(Account *dest_acc, float amount) = 0;
-
-    void get_common_inputs(Person * acc_holder);
+    void get_common_inputs(Person *acc_holder);
     virtual void get_specific_inputs() = 0;
     virtual void display_info() = 0;
 
--- src/Account/Account.cpp
+++ src/Account/Account.cpp
@@ -1,7 +1,7 @@
 #include "../../include/Account/Account.h"
 #include "../../include/Utility/UI/Utility_UI.h"
 
-Account::Account() : acc_no(""), balance(0.0), type(""), pin(""), owner(nullptr) {}
+Account::Account() : acc_no(""), balance(0.0), type(""), pin(""), owner(nullptr), onetime_transaction_limit(10000.0), minimum_transaction_limit(100.0) {}
 
 // Getters
 string Account::getAccNo() { return this->acc_no; }
@@ -9,6 +9,8 @@ double Account::getBalance() { return this->balance; }
 string Account::getType() { return this->type; }
 string Account::getPin() { return this->pin; }
 Person *Account::getOwner() { return this->owner; }
+double Account::getOneTimeTransactionLimit() { return this->onetime_transaction_limit; }
+double Account::getMinimumTransactionLimit() { return this->minimum_transaction_limit; }
 
 // Setters
 void Account::setAccNo(const string &acc_no) { this->acc_no = acc_no; }
@@ -16,19 +18,8 @@ void Account::setBalance(double balance) { this->balance = balance; }
 void Account::setType(const string &type) { this->type = type; }
 void Account::setPin(const string &pin) { this->pin = pin; }
 void Account::setOwner(Person *owner) { this->owner = owner; }
-
-// Transaction methods
-void Account::deposit(float amount)
-{
-}
-
-void Account::withdraw(float amount)
-{
-}
-
-void Account::transfer(Account *toAccount, float amount)
-{
-}
+void Account::setOneTimeTransactionLimit(double amount) { this->onetime_transaction_limit = amount; }
+void Account::setMinimumTransactionLimit(double amount) { this->minimum_transaction_limit = amount; }
 
 void Account::get_common_inputs(Person *acc_holder)
 {
@@ -39,7 +30,6 @@ void Account::get_common_inputs(Person *acc_holder)
 
 static void login()
 {
-    
 }
 
 Account::~Account() {}
